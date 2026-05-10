f1c8de39c82d414556d0cb0431b3a47b59711cab
divyansh-prks <divyansh2375@gmail.com>
2026-05-10 15:24:12 +0530

    getting data from extension back to vscode

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "Microcks" is now active!');
-
+  
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
@@ -27,60 +27,81 @@ export function activate(context: vscode.ExtensionContext) {
 
         
       );
-    
+      
       // Display a message box to the user
-
+      
       panel.webview.html = `<html>
-    <body>
+      <body>
       <h1>Hello Pulse API</h1>
       <p> I have setuped the most important thing here </p> 
-       <br>
-       <div class="main" data-vscode-context='{"webviewSection": "main", "mouseCount": 4}'>
-  <h1>Cat Coding</h1>
-
-    <h2>Pulse API</h2>
-
-  <input
-    type="text"
-    id="url"
-    placeholder="Enter URL"
-    style="width: 400px;"
-  />
-
-  <button onclick="sendRequest()">
-    Send
-  </button>
-
-  <pre id="result"></pre>
-  <textarea data-vscode-context='{"webviewSection": "editor", "preventDefaultContextMenuItems": true}'></textarea>
+      <br>
+      <div class="main" data-vscode-context='{"webviewSection": "main", "mouseCount": 4}'>
+      <h1>Cat Coding</h1>
+      
+      <h2>Pulse API</h2>
+      
+      <input
+      type="text"
+      id="url"
+      placeholder="Enter URL"
+      style="width: 400px;"
+      />
+      
+      <button onclick="sendRequest()">
+      Send
+      </button>
+      
+      <pre id="result"></pre>
+      <textarea data-vscode-context='{"webviewSection": "editor", "preventDefaultContextMenuItems": true}'></textarea>
       <form action="/action_page.php">
-  <label for="fname">Request Url:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Payload:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
-  <input type="submit" value="Submit">
-</form> 
- 
-<script>
+      <label for="fname">Request Url:</label><br>
+      <input type="text" id="fname" name="fname" value="John"><br>
+      <label for="lname">Payload:</label><br>
+      <input type="text" id="lname" name="lname" value="Doe"><br><br>
+      <input type="submit" value="Submit">
+      </form> 
+      
+      
+      
+      
+      
+      <script>
+      const vscode = acquireVsCodeApi()
+      
+      
+      function sendRequest() {
+        
+      const url =
+      document.getElementById('url').value;
+      
+      alert(url);
+      const name = "divyansh"
+      
+      
+      vscode.postMessage({
+        type : 'sendRequest' ,
+        url : url , 
+        divname : name
+        
+        })
+        }
+        
+        </script>
+        </body>
+        </html>`;
+        panel.webview.onDidReceiveMessage(
+        async (message) => {
+        
+        console.log(message);
+        }
+        
+        )
+        
+},
+);
 
-function sendRequest() {
 
-const url =
-  document.getElementById('url').value;
-
-alert(url);
-const name = "divyansh"
-
-}
-
-</script>
-    </body>
-  </html>`;
-
-    },
-  );
-
-  context.subscriptions.push(disposable);
+context.subscriptions.push(disposable);
 }
 
 
